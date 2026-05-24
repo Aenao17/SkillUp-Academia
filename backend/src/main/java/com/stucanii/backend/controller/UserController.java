099f46ec4f955666fbd9f3fc547fc5253f1aca78
andrapavel <andrapavel2003@gmail.com>
2026-05-24 16:01:34 +0300

    fixed minor bugs

--- backend/src/main/java/com/stucanii/backend/controller/UserController.java
+++ backend/src/main/java/com/stucanii/backend/controller/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -50,5 +51,14 @@ public class UserController {
         return ResponseEntity.ok(
                 moduleService.getAllModules(userDetails)
         );
+    }
+
+    @DeleteMapping("/me")
+    public ResponseEntity<Void> deleteCurrentUser(
+            @AuthenticationPrincipal UserDetails userDetails
+    ) {
+        UserDTO dto = userService.getCurrentUser(userDetails.getUsername());
+        userService.delete(dto.userId());
+        return ResponseEntity.noContent().build();
     }
 }
